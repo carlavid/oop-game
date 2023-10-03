6e9fcf8d8d598be24bd935cf708633f7f73ac1f4
Carla Vidales <carlavidales.ca@gmail.com>
2023-10-03 10:29:29 -0700

    Add event listener for the start button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+/**
+ * create a new instance of the Game class and add event 
+ * listeners for the start button and onscreen keyboard buttons
+ */
+
+let game;
+
+const startBtn = document.querySelector("#btn__reset");
+startBtn.addEventListener("click", () => {
+    game = new Game();
+    game.startGame();
+})
