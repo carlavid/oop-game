3e17016615538a4865717d2fd506174ec28eee2e
Carla Vidales <carlavidales.ca@gmail.com>
2023-10-03 10:21:38 -0700

    Create method to display phrase on game board

--- js/Phrase.js
+++ js/Phrase.js
@@ -10,4 +10,27 @@ class Phrase {
     constructor(phrase) {
        this.phrase = phrase.toLowerCase(); 
     }
+
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() {
+        const phraseUL = document.querySelector("#phrase").firstElementChild;
+
+        for (let i = 0; i < this.phrase.length; i++) {
+            const li = document.createElement("li");
+            phraseUL.appendChild(li);
+            let char = this.phrase[i];
+
+            if (char === " ") {
+                li.className = "space";
+                li.textContent = " ";
+            } else if (char !== " ") {
+                li.className = `hide letter ${char}`;
+                li.textContent = char;
+            } 
+        };
+        return phraseUL;
+    }
 };
+
