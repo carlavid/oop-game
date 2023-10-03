31f4decc8fd12d410bf260f134bed80411c7c36d
Carla Vidales <carlavidales.ca@gmail.com>
2023-10-02 17:20:44 -0700

    Define Phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,13 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+/**
+ * define a Phrase class to handle the creation of phrases 
+ * (serve as blueprint for creating phrase objects)
+ */
+class Phrase {
+    constructor(phrase) {
+       this.phrase = phrase.toLowerCase(); 
+    }
+};
